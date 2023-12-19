23b8ddf8e00b6e14dc51718d247021f2fa466c0a
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-19 17:49:55 +0100

    adding the necessary images

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,6 +19,11 @@ const Hero = () => {
           handleClick={handleScroll}
            />
         </div>
+        <div className="hero__image-container">
+           <div className="hero__image">
+             <Image src=""/> 
+           </div>
+        </div>
     </div>
   )
 }
