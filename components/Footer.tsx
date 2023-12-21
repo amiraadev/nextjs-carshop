232282b82a767f81df05d471a94a22164222ca37
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-21 15:32:47 +0100

    done with the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,27 +1,52 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-Link
-const Footer = () => {
-  return (
-    <footer className='flex flex-col text-slate-200 mt-5 border-t border-gray-700'>
-      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
-        <div className="flex flex-col justify-start items-start gap-6">
-          <Image
-           src='/logo-green-car-2.png'
-           alt='logo image'
-           width={80}
-           height={18}
-           />
-           <p className='text-base text-slate-500'>
-                Car Hub 
-               <br/>
-               All rights reserved &copy;
-           </p>
-        </div>
-      </div>
-    </footer>
-  )
-}
+import Image from "next/image";
+import Link from "next/link";
 
-export default Footer
+import { footerLinks } from "@/constants";
+
+const Footer = () => (
+  <footer className='flex flex-col text-black-100  mt-5 border-t border-gray-800'>
+    <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
+      <div className='flex flex-col justify-start items-start gap-6'>
+        <Image src='/logo-green-car-2.png' alt='logo' width={80} height={18} className='object-contain' />
+        <p className='text-base text-gray-700'>
+          Carhub 2023 <br />
+          All Rights Reserved &copy;
+        </p>
+      </div>
+
+      <div className="footer__links">
+        {footerLinks.map((item) => (
+          <div key={item.title} className="footer__link">
+            <h3 className="font-bold text-slate-300">{item.title}</h3>
+            <div className="flex flex-col gap-5">
+              {item.links.map((link) => (
+                <Link
+                  key={link.title}
+                  href={link.url}
+                  className="text-gray-500"
+                >
+                  {link.title}
+                </Link>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+
+    <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-800 sm:px-16 px-6 py-10'>
+      <p  className="text-gray-500">@2023 CarHub. All rights reserved</p>
+
+      <div className="footer__copyrights-link">
+        <Link href="/" className="text-gray-500">
+          Privacy & Policy
+        </Link>
+        <Link href="/" className="text-gray-500">
+          Terms & Condition
+        </Link>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
