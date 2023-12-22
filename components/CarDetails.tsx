b6678fecaf421bb0077a8b07a98f82acf9df10ce
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-22 14:42:04 +0100

    getting the keys of an object with Object.entries(car)

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -91,6 +91,12 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
 											</div>
 										</div>
 									</div>
+									<div className="flex-1 flex flex-col gap-2">
+										<h2 className="font-semibold text-xl capitalize">
+											{car.make} {car.model}
+										</h2>
+										<div className="mt-3 flex flex-wrap gap-4"></div>
+									</div>
 								</Dialog.Panel>
 							</Transition.Child>
 						</div>
